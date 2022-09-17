6da69c13376af50cb886c0be0538e68778590110
LiJoBaZar <lijobazar@gmail.com>
2022-09-17 09:18:00 -0700

    correction Betty Style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,6 @@
 #include "binary_trees.h"
 
  /**
- *binary_tree_node - Creates a binary tree node.
  *
  *@parent: Pointer to the parent node of the node to create.
  *@value: Value to be assigned in the new node.
